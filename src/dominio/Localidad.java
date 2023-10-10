2fa1768c161c3887d9a04f4fdc3383cc5e22af92
Pablo Corzo <pablo.corzocorella@eps.ceu.es>
2023-10-10 17:08:59 +0200

    creado readme y src

---
+++ src/dominio/Localidad.java
@@ -0,0 +1,19 @@
+package dominio;
+public class Localidad {
+	private String nombre;
+	private int numero_habitantes;
+	public Localidad(String nombre,int numero_habitantes){
+		this.nombre = nombre;
+		this.numero_habitantes = numero_habitantes;
+	}
+	public setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public setNumero_habitantes(numero_habitantes){
+		this.numero_habitantes = numero_habitantes;
+	}
+	public getNombre(){
+		return 
+	}
+	
+}

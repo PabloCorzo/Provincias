c80c230cb4ff5d956e55afc0c619c0ea26b13a10
PabloCorzo <pablo.corzocorella@usp.ceu.es>
2023-10-14 21:23:21 +0200

    completado el dominio

--- src/dominio/Municipio.java
+++ src/dominio/Municipio.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 
 public class Municipio {
     private int habitantes_municipio;
-    ArrayList <Localidad> localidades = new ArrayList<>();
+    private String nombre_municipio;
+    static ArrayList <Localidad> localidades = new ArrayList<>();
     public int contarHabitantesMunicipio(){
         this.habitantes_municipio = 0;
         for(Localidad localidad : localidades ){
@@ -11,11 +12,28 @@ public class Municipio {
         }
         return this.habitantes_municipio;
     }
+    public String getNombre(){
+        return this.nombre_municipio;
+    }
     public String toString(){
-        ArrayList <String> nombres_localidades = new ArrayList<>();
-        for(String nombre : nombres_localidades){
-            nombres_localidades.add(nombre);
-            System.out.println(nombre);
-        }
+        String result = "El municipio de " + this.nombre_municipio + " esta compuesto por: ";
+        for(Localidad localidad : localidades) {
+            result += (localidad.getNombre());
+            result += (", ");
+        } 
+        result += "y tiene " + this.contarHabitantesMunicipio() + " habitantes";
+        return result;
     } 
+    public Municipio(String nombre, Localidad loc1,Localidad loc2){
+        this.nombre_municipio = nombre;
+        localidades.add(loc1);
+        localidades.add(loc2);
+        this.habitantes_municipio = contarHabitantesMunicipio();
+    }
+    public static void main(String[] args) {
+        Localidad local1 = new Localidad("john", 2);
+        Localidad local2 = new Localidad("peppord", 3);
+        Municipio john = new Municipio("John Peppord", local1,local2);
+        System.out.println(john.toString());
+    }
 }
--- src/dominio/Provincia.java
+++ src/dominio/Provincia.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 
 public class Provincia {
     private int habitantes_provincia;
-    ArrayList <Municipio> municipios = new ArrayList<>();
+    private String nombre_provincia;
+    static ArrayList <Municipio> municipios = new ArrayList<>();
     
     public int contarHabitantesProvincia(){
         this.habitantes_provincia = 0;
@@ -12,4 +13,22 @@ public class Provincia {
         }
         return this.habitantes_provincia;
     }
+    public String toString(){
+        String result = "El municipio de " + this.nombre_provincia + " esta compuesto por: ";
+        for(Municipio municipio: municipios) {
+            result += (municipio.getNombre());
+            result += (", ");
+        } 
+        result += " y tiene " + this.contarHabitantesProvincia() + " habitantes";
+        return result;
+    } 
+    public Provincia(String nombre, Municipio mun1, Municipio mun2){
+        this.nombre_provincia = nombre;
+        municipios.add(mun1);
+        municipios.add(mun2);
+        this.habitantes_provincia = contarHabitantesProvincia();
+    }
+    public static void main(String[] args) {
+    }
 }
+

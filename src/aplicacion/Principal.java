f0a58ca1d4022eb8ac6e1642f7542982968b529e
Pablo Corzo <pablo.corzocorella@eps.ceu.es>
2023-10-17 12:01:46 +0200

    cambios finales

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -2,11 +2,15 @@ package aplicacion;
 import dominio.*;
 public class Principal {
     public static void main(String[] args) {
-        Localidad local1 = new Localidad("Orcasitas", 2);
-        Localidad local2 = new Localidad("Vallecas", 3);
-        Municipio madrid = new Municipio("Madrid", local1,local2);
+        Localidad local1 = new Localidad("Valencia", 2);
+        Localidad local2 = new Localidad("Castellon", 3);
         System.out.println(local1.toString());
         System.out.println(local2.toString());    
+        Municipio madrid = new Municipio("Madrid", local1,local2);
         System.out.println(madrid.toString());
+        Localidad local3 = new Localidad("Alicante", 9);
+        madrid.addLocalidad(local3);
+        System.out.println(madrid.toString());
+
     }
 }
